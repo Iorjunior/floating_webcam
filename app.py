03e2bf0a33abdec415c9116c6807042cdbbe00d5
Junior Limm <juniorcoda@gmail.com>
2021-08-19 13:24:27 -0300

    Add tranform shape and finish app.

--- app.py
+++ app.py
@@ -5,7 +5,15 @@ import cv2
 
 class Application():
     def __init__(self,master):
-        self.master = master  
+        self.master = master
+        self.master.protocol("WM_DELETE_WINDOW", self.finishe_app)
+
+        self._DEFAULT_SIZE = 40
+        self._ZOOM_SIZE = 60
+        self._SHAPE = 'square'
+        self._BORDER_COLOR = '#ffffff' 
+
+        self.is_finished = False
         
         self.label_content = Label(self.master)
         self.label_content.pack()
@@ -16,18 +24,22 @@ class Application():
         self.reed_webcam_thread.start()
 
     def reed_webcam(self):
-        while True:
+        while (self.is_finished == False):
             status, frame = self.webcam_device.read()
 
             if status:
+                frame = self.mirrored_frame(frame)
                 frame = self.convert_frame_to_correct_color(frame)
                 frame = self.convert_frame_array_to_image(frame)
                 frame = self.resize_frame(frame,40)
+                frmae = self.transform_shape(frame,self._SHAPE)
                 frame = self.convert_image_to_tk_image(frame)
 
                 self.label_content.configure(image=frame)
                 self.label_content.image = frame         
-    
+        
+        self.master.quit()
+
     def resize_frame(self,frame,porcent):
         
         x = int(frame.size[0] * porcent / 100)
@@ -51,12 +63,34 @@ class Application():
         return tk_image
 
     def mirrored_frame(self,frame):
-        mirrored_frame = cv2.flip(frame)
+        mirrored_frame = cv2.flip(frame,1)
         return mirrored_frame
+    
+    def transform_shape(self,frame,shape):
+        if shape == 'square':
+            x,y = frame.size
+            frame_square = frame.crop((int(x*self._DEFAULT_SIZE/100),0,int(x-(x*self._DEFAULT_SIZE/100)),y))
 
+            return frame_square
+        else:
+            return frame
+
+        
+         
+
+    def finishe_app(self):
+        self.is_finished = True
     
 
 if __name__ == "__main__":
+    try:
+        with open('config.txt') as f:
+            print(f)
+    except:
+        pass
+
+    
+    
     root = Tk()
     app = Application(root)
     root.mainloop()
